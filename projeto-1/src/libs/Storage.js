7890c4e95e832221904285c200a3572d074980ff
Ayrton Teshima <ayrtonteshima@gmail.com>
2022-02-27 14:56:59 -0300

    aula 30: findOneAndUpdate

--- projeto-1/src/libs/Storage.js
+++ projeto-1/src/libs/Storage.js
@@ -17,19 +17,18 @@ export class Storage {
 
   async save(data) {
     const id = this.#generateId();
-    const key = `${this.table}-${id}`;
     
     const newData = {
       id,
       ...data,
     }
 
-    this.#storage.setItem(key, JSON.stringify(newData));
+    this.#storage.setItem(this.#getKey(id), JSON.stringify(newData));
     return data;
   }
 
   async findOne(id) {
-    return JSON.parse(this.#storage.getItem(`${this.table}-${id}`));
+    return JSON.parse(this.#storage.getItem(this.#getKey(id)));
   }
 
   async findAll() {
@@ -42,7 +41,23 @@ export class Storage {
         ))
   }
 
-  async findOneAndUpdate(id, data) {}
+  async findOneAndUpdate(id, data) {
+    const item = await this.findOne(id);
+    
+    if (!item) {
+      throw new Error(`Registro "${id}" não encontrado em ${this.table}`);
+    }
+
+    const newData = {
+      ...item,
+      ...data,
+      id: item.id,
+    };
+
+    this.#storage.setItem(this.#getKey(id), JSON.stringify(newData));
+
+    return this.findOne(id);
+  }
 
   async remove(id) {}
 
@@ -55,6 +70,10 @@ export class Storage {
           .substring(1);
     }
     return `${gen()}${gen()}-${gen()}${gen()}`;
+  }
+
+  #getKey(id) {
+    return `${this.table}-${id}`;
   }
 
 }

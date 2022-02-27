6a45ba50926397ff757c630965ae09289605f388
Ayrton Teshima <ayrtonteshima@gmail.com>
2022-02-27 12:06:58 -0300

    aula 28: inicio storage + save

--- projeto-1/src/libs/Storage.js
+++ projeto-1/src/libs/Storage.js
@@ -1,5 +1,32 @@
-class Storage {
-  async save(data) {}
+export class Storage {
+  #table;
+  #storage;
+
+  constructor(table) {
+    this.#table = table;
+    this.#storage = window.localStorage;
+  }
+
+  get table() {
+    return this.#table;
+  }
+
+  set table(table) {
+    this.#table = table;
+  }
+
+  async save(data) {
+    const id = this.#generateId();
+    const key = `${this.table}-${id}`;
+
+    const newData = {
+      id,
+      ...data,
+    }
+
+    this.#storage.setItem(key, JSON.stringify(newData));
+    return data;
+  }
 
   async findOne(id) {}
 
@@ -10,5 +37,14 @@ class Storage {
   async remove(id) {}
 
   async removeAll() {}
+
+  #generateId() {
+    let gen = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    }
+    return `${gen()}${gen()}-${gen()}${gen()}`;
+  }
 
 }

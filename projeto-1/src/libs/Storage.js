b9b4496a8d0320e87432059c772c6d2e8275f09e
Ayrton Teshima <ayrtonteshima@gmail.com>
2022-02-27 11:09:02 -0300

    aula 26 - Storage

---
+++ projeto-1/src/libs/Storage.js
@@ -0,0 +1,14 @@
+class Storage {
+  async save(data) {}
+
+  async findOne(id) {}
+
+  async findAll() {}
+
+  async findOneAndUpdate(id, data) {}
+
+  async remove(id) {}
+
+  async removeAll() {}
+
+}
